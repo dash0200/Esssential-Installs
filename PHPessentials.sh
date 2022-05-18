cd8b9eb831e2a9d1a0573c9c658dbd7f00d089e3
Darshan V <41298533+dash0200@users.noreply.github.com>
2022-05-18 10:41:44 +0530

    Add files via upload

---
+++ PHPessentials.sh
@@ -0,0 +1,6 @@
+sudo apt-get install php-curl
+sudo apt install php-xml
+sudo apt install php-mysql
+sudo apt-add-repository ppa:ondrej/php
+sudo apt install php8.1-mysql
+sudo apt install php7.2-mysql
